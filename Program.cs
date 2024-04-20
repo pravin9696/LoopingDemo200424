bdab4716f4d36c84cae8abc1d12609a7066e4df9
pravin9696 <pravingaikwad.sibar@gmail.com>
2024-04-20 11:59:03 +0530

    new code added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,68 @@
-﻿internal class Program
+﻿using LoopingDemo200424;
+using System.Diagnostics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("new message added");
+       // create object of myclass12
+
+         myclass12 obj1=new myclass12();
+
+        // call method show using object of myclass12
+        //obj1.xyz();
+        //obj1.show();
+        //obj1.checkEven();
+
+        //Console.WriteLine("end of main method...");
+
+        //int x = 78;
+
+        //if (x==1)
+        //{
+        //    Console.WriteLine("one");
+        //}
+
+        //else if(x==2)
+        //{
+        //    Console.WriteLine("two");
+        //}
+
+
+        //Console.WriteLine("next line after else if ladder");
+
+        //-------------------------------------------------------------------------------------------------//
+        //nested if or nested if else statements
+        /*
+          if(Condition)
+              {
+                 if (cond)
+                 {
+
+                 }       
+              }
+          else
+        {
+          
+        }
+        */
+        int n = 1;
+
+        if (n<3)
+        {
+            if (n%2==0)
+            {
+                Console.WriteLine("number is even and less than 3");
+            }
+            else
+            {
+                Console.WriteLine("no is odd and less than 3");
+            }
+        }
+        else
+        {
+            Console.WriteLine("no is greater than 3....");
+        }
+
     }
 }
--- myclass12.cs
+++ myclass12.cs
@@ -2,15 +2,70 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace LoopingDemo200424
 {
+    
     internal class myclass12
     {
+        private int x = 5;
+
+        public void mymsg()
+        {
+            Console.WriteLine("this is msg from xyz");
+        }
+        public void xyz()
+        {
+            int z = x;
+            mymsg();
+        
+        }
         public void show()
         {
-            Console.WriteLine("show called");
+            mymsg();
+            int p = x;
+            x = x + 4;
+            // conditional statements
+
+            // if
+            //if else
+            //if else ladder  //else if ladder
+            //nested if
+
+            //switch
+
+            //if conditional statement
+
+            //synatx
+            /*
+                if(condition)
+                  {
+
+
+                  }
+             */
+            int a = 5;
+            if(a>10)
+            {
+                Console.WriteLine("greater");
+            }
+            Console.WriteLine("next line....");
+        }
+        public void checkEven()
+        {
+            int n;
+            Console.WriteLine("Enter number:");
+            n=Convert.ToInt32( Console.ReadLine());
+            if (n%2==0)
+            {
+                Console.WriteLine(n+" is even");
+            }
+            else
+            {
+                Console.WriteLine(n+" is odd number");
+            }
         }
     }
 }
