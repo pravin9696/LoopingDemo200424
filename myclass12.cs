fd9fdf1287463847e1c1a21582b63b1ea2025e2e
pravin9696 <pravingaikwad.sibar@gmail.com>
2024-04-20 10:39:11 +0530

    new class and some lines added into project

---
+++ myclass12.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingDemo200424
+{
+    internal class myclass12
+    {
+        public void show()
+        {
+            Console.WriteLine("show called");
+        }
+    }
+}
